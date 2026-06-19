4706a04e636698324ccf3114a6e22f3732359a66
Vũ Văn Cường <you@example.com>
2026-06-19 22:43:18 +0700

    update

--- FirstWebMVC/Models/CodeFrist/Category.cs
+++
@@ -1,8 +0,0 @@
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    // Navigation property (1 -> nhiều)
-    public ICollection<Product> Products { get; set; }
-}
