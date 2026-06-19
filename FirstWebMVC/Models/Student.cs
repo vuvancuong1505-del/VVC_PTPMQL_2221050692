26c4dfbbad7572cf5e2301a0ec7d3985e23a9f16
Vũ Văn Cường <you@example.com>
2026-06-19 23:27:55 +0700

    BaiThucHanhSo6

--- FirstWebMVC/Models/Student.cs
+++ FirstWebMVC/Models/Student.cs
@@ -5,7 +5,12 @@ namespace FirstWebMVC.Models
     public class Student
     {
         [Key]
+        [Required(ErrorMessage = "Mã sinh viên không được để trống")]
+        [Display(Name = "Mã sinh viên")]
         public string StudentCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [Display(Name = "Họ và tên")]
         public string FullName { get; set; } = string.Empty;
     }
 }
